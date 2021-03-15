20186306ed1050a1cb0f22d570234d589baeaa7a
RAMAN BHADAURIA <257ramanrb@gmail.com>
2021-03-15 16:06:08 +0530

    Implement bubbleSortOptimized()

--- sorting/bubble_sort.java
+++ sorting/bubble_sort.java
@@ -8,7 +8,7 @@ class Solution
     {
         for(int i = 0; i < n-1; i++)
         {
-            for(int j = 0; j < n - i - 1; j++)
+              for(int j = 0; j < n - i - 1; j++)
               {
                   if(arr[j] > arr[j+1])
                   {
@@ -17,6 +17,26 @@ class Solution
                      arr[j+1] = tmp;
                   }
               }
+        }
+    }
+    
+    void bubbleSortOptimized(int arr[], int n)
+    {
+        for(int i = 0; i < n-1; i++)
+        {
+              boolean isSwapped = false;
+              for(int j = 0; j < n - i - 1; j++)
+              {
+                  if(arr[j] > arr[j+1])
+                  {
+                     int tmp = arr[j];
+                     arr[j] = arr[j+1];
+                     arr[j+1] = tmp;
+                     isSwapped = true;
+                  }
+              }
+              if(!isSwapped) 
+                  break;    // means array already sorted now.
         }
     }
 }
